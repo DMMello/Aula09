67cc18106ba2eea59c0874ff1e02a557ce81d795
DMMello <diogommello@gmail.com>
2024-10-08 20:41:14 -0300

    teste try

---
+++ Aula09/exemplo.py
@@ -0,0 +1,16 @@
+
+
+try:
+    n = int(input('Informe um numero :'))
+except:
+    print('Erro!')
+
+
+
+# try:
+#     n = int(input('Informe um numero :'))
+# except ValueError as e:
+#     print(f'{e}')
+# except KeyboardInterrupt:
+#     print('\nO Usuário cancelou a operacao')
+
